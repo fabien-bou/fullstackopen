52401044af7bc2c553123760c532a5b36f90c02f
chubfr <chubfr@yahoo.fr>
2025-12-19 17:06:57 +0100

    feat(part1): unicafe init

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+const App = () => {
+  // enregistrer les clics de chaque bouton dans un état différent
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      code here
+    </div>
+  )
+}
+
+export default App
