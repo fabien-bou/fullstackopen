0f9ec19b8597a490b6220b7a8aad939e778ab60b
chubfr <chubfr@yahoo.fr>
2025-12-24 11:13:41 +0100

    feat(part2): course info 2.2

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -6,6 +6,12 @@ const Content = ({parts}) =>  (
   </div>
 )
 
+const Total = ({parts}) => (
+  <p>
+    <b>Number of exercises {parts.reduce((sum, part) => sum + part.exercises, 0)}</b>
+  </p>
+)
+
 const Part = ({part}) => (
   <p>
     {part.name} {part.exercises}
@@ -17,6 +23,7 @@ const Course = ({course}) => {
     <div>
       <Header course={course} />
       <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
