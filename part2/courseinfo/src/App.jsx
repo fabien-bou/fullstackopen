3e93bcddb14013905107d617e854b07fa211a0b0
chubfr <chubfr@yahoo.fr>
2025-12-24 11:14:33 +0100

    feat(part2): course info 2.3

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -6,15 +6,15 @@ const Content = ({parts}) =>  (
   </div>
 )
 
-const Total = ({parts}) => (
-  <p>
-    <b>Number of exercises {parts.reduce((sum, part) => sum + part.exercises, 0)}</b>
-  </p>
-)
-
 const Part = ({part}) => (
   <p>
     {part.name} {part.exercises}
+  </p>
+)
+
+const Total = ({parts}) => (
+  <p>
+    <b>Number of exercises {parts.reduce((sum, part) => sum + part.exercises, 0)}</b>
   </p>
 )
 
